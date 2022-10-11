c2a9ad860cbad9ea2c2538f8b2fa5c9312f6ff62
NadaBouyahya <92028638+NadaBouyahya@users.noreply.github.com>
2022-10-11 16:51:36 +0100

    Create projet1.sql

---
+++ projet1.sql
@@ -0,0 +1,12 @@
+DELETE DATABASE IF EXISTS;
+CREATE DATABASE projet1;
+USE projet1;
+
+CREATE TABLE `promotion` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(250) NOT NULL
+) 
+
+--
+-- Dumping data for table `promotion`
+--
