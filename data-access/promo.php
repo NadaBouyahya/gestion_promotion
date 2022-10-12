debe416f3cbf548da3766fedbd7d8efce428a35d
NadaBouyahya <92028638+NadaBouyahya@users.noreply.github.com>
2022-10-12 13:52:47 +0100

    commit

--- data-access/promo.php
+++ data-access/promo.php
@@ -9,16 +9,28 @@
         public $id;
         public $name;
 
-        //
+    
         function __construct($id, $name) {
             $this->id = $id;
             $this->name = $name;
             $this->conn = new mysqli($this->host, $this->user, $this->password, $this->db);
         }
 
+        function select_promo(){
+            $select = "SELECT * FROM promotion";
+            return $this->conn->query($select);
+        }
+
         function add_promo(){
             $add = "INSERT INTO promotion(name) VALUES ('$this->name') ";
             $this->conn->query($add);
         }
+
+        function delete_promo(){
+            $delete = "DELETE FROM promotion WHERE 
+            id = $this->id";
+            $this->conn->query($delete);
+        }
+
     }
 ?>
